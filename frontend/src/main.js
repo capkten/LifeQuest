e12c7f831daddf8c89b51b6bd8defedc09903f09
capkin <capkten@gmail.com>
2026-06-10 10:11:11 +0800

    fix(frontend): use correct v-md-editor hljs config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,10 +8,10 @@ import VMdEditor from '@kangc/v-md-editor'
 import '@kangc/v-md-editor/lib/style/base-editor.css'
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
 import '@kangc/v-md-editor/lib/theme/style/github.css'
-import Prism from 'prismjs'
+import hljs from 'highlight.js'
 
 VMdEditor.use(githubTheme, {
-  Prism
+  Hljs: hljs
 })
 
 const app = createApp(App)
